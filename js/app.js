ec5e0eaf7454f9f0c3fff606e91e1d64bf612ac4
daimonti121 <damios.1812@gmail.com>
2021-12-16 15:19:41 +0300

    text-translate 3

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ window.HUB_EVENTS={ASSET_ADDED:"ASSET_ADDED",ASSET_DELETED:"ASSET_DELETED",ASSET
 
 var colors = ["#3a1149", "#f30000", "#24177c", "#ec710b",
              "#3a1149", "#f30000", "#24177c", "#ec710b"];
-var prizes = ["          Tente           novamente", " 100% up to  1200 BRL", "20 RG", "100 RG", "Agora em", "  50% up to   500 BRL", "75 RG", "   75% up to   500 BRL"];
+var prizes = ["          Tente           novamente", "100% de até  1200 R$", "20 RG", "100 RG", "Agora em", " 50% de até   500 R$", "75 RG", " 75% de até   500 R$"];
 
 var startAngle = 0*Math.PI/180;
 var arc = (2 * Math.PI) / colors.length;
